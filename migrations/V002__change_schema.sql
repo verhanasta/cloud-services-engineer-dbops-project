c99ac27b7a585e0204ec26ffd7596b889de54eba
verhanasta <verhanasta@yandex.ru>
2025-09-25 03:16:32 +0300

    Test1.12

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,16 +1,16 @@
----- Обработка таблицы product: Добавление колонки "price" с типом число с плавающей точкой и Назначение колонки "id" первичным ключом таблицы
---ALTER TABLE product
---    ADD price DOUBLE PRECISION,
---    ADD PRIMARY KEY (id);
----- Обработка таблицы orders. Добавление колонки "date_created" с типом дата и назначение колонки "id" первичным ключом таблицы
---ALTER TABLE orders
---    ADD date_created DATE,
---    ADD PRIMARY KEY (id);
---
----- Добавление внешних ключей для целостности информации
---ALTER TABLE order_product
---    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id),
---    ADD CONSTRAINT fk_orders_id FOREIGN KEY (order_id) REFERENCES orders (id);
+-- Обработка таблицы product: Добавление колонки "price" с типом число с плавающей точкой и Назначение колонки "id" первичным ключом таблицы
+ALTER TABLE product
+    ADD price DOUBLE PRECISION,
+    ADD PRIMARY KEY (id);
+-- Обработка таблицы orders. Добавление колонки "date_created" с типом дата и назначение колонки "id" первичным ключом таблицы
+ALTER TABLE orders
+    ADD date_created DATE,
+    ADD PRIMARY KEY (id);
+
+-- Добавление внешних ключей для целостности информации
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id),
+    ADD CONSTRAINT fk_orders_id FOREIGN KEY (order_id) REFERENCES orders (id);
 --
 ---- Удаление таблиц
 --DROP TABLE product_info;

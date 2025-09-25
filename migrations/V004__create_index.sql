409ec347c627f1c16893a0c543c6bbfe59cbc932
verhanasta <verhanasta@yandex.ru>
2025-09-25 03:44:41 +0300

    Test1.20

--- migrations/V004__create_index.sql
+++ migrations/V004__create_index.sql
@@ -1,2 +1,2 @@
---CREATE INDEX idx_order_product_order_id ON order_product(order_id);
---CREATE INDEX idx_orders_status_date ON orders(status, date_created);
+CREATE INDEX idx_order_product_order_id ON order_product(order_id);
+CREATE INDEX idx_orders_status_date ON orders(status, date_created);
